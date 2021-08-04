14460da7cbc961dd08bad7dd7c4c1a65b41656c7
Adina Henderson <adinahenderson.mail@gmail.com>
2021-08-04 18:53:13 -0400

    added audhMiddleware function as context in server config

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const path = require('path');
 const db = require("./config/connection");
-const routes = require("./routes");
+//const routes = require("./routes");
+
 // Import ApolloServer class
 const {ApolloServer} = require('apollo-server-express');
-
-// Import GraphQL schema
+const {authMiddleware} = require('./utils/auth');
+// Import GraphQL schema typeDefs and resolvers
 const { typeDefs, resolvers} = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Apollo server
+// Apollo server created to pass schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
+// Integrate Apollo server with Express
 server.applyMiddleware({ app });
-//
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,7 +30,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
