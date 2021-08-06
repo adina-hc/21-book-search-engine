9c7753876b8da512b9e003edd6d53054652c6d14
Adina Henderson <adinahenderson.mail@gmail.com>
2021-08-06 11:24:36 -0400

    cleared bug related to variable not used

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -78,7 +78,7 @@ const SearchBooks = () => {
       const { data } = await saveBook({
         variables: {bookInfo: {...bookToSave}}
       });
-
+      console.log(data);
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
