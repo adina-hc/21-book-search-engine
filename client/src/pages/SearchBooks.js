ba8e6a3ca0aabd6c410e4a50d44d7adda4da92c4
Adina Henderson <adinahenderson.mail@gmail.com>
2021-08-07 14:51:36 -0400

    added packages to json

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,7 +8,6 @@ import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { SAVE_BOOK } from "../utils/mutations";
 import { useMutation } from '@apollo/react-hooks';
 
-
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
